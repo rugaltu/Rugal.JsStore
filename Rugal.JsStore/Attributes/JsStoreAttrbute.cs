2542897a20cd8f948a3b42f894abac9ba197b0d7
Rugal Tu <john2653589@gmail.com>
2024-04-29 22:57:28 +0800

    Rugal Update

--- Rugal.JsStore/Attributes/JsStoreAttrbute.cs
+++ Rugal.JsStore/Attributes/JsStoreAttrbute.cs
@@ -5,7 +5,7 @@ using Rugal.JavaScriptStore.Service;
 namespace Rugal.JavaScriptStore.Attributes
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public partial class JsDIAttribute : Attribute, IActionFilter
+    public partial class JsStoreAttribute : Attribute, IActionFilter
     {
         public void OnActionExecuted(ActionExecutedContext context)
         {
@@ -15,14 +15,14 @@ namespace Rugal.JavaScriptStore.Attributes
         {
             var HttpContext = context.HttpContext;
             var Query = HttpContext.Request.Query;
-            var JsDIService = HttpContext.RequestServices.GetService<JsStoreService>();
+            var JsStoreService = HttpContext.RequestServices.GetService<JsStoreService>();
             foreach (var Item in Query)
             {
                 var Value = Item.Value.ToString();
                 if (string.IsNullOrWhiteSpace(Value))
                     Value = null;
 
-                JsDIService.AddQuery(Item.Key, Value);
+                JsStoreService.AddQuery(Item.Key, Value);
             }
         }
     }
--- Rugal.JsStore/Extention/StartupExtention.cs
+++ Rugal.JsStore/Extention/StartupExtention.cs
@@ -8,9 +8,23 @@ namespace Rugal.JavaScriptStore.Extention
     {
         public static IServiceCollection AddJsStore(this IServiceCollection Services)
         {
+            BaseAddSetting(Services);
             Services.AddScoped<JsStoreService>();
-            Services.AddScoped<JsStoreSetting>();
             return Services;
+        }
+
+        public static IServiceCollection AddJsStore(this IServiceCollection Services, Action<JsStoreSetting> SettingFunc = null)
+        {
+            BaseAddSetting(Services, SettingFunc);
+            Services.AddScoped<JsStoreService>();
+            return Services;
+        }
+        private static void BaseAddSetting(IServiceCollection Services, Action<JsStoreSetting> SettingFunc = null)
+        {
+            var Setting = new JsStoreSetting();
+            SettingFunc?.Invoke(Setting);
+
+            Services.AddSingleton(Setting);
         }
     }
 }
